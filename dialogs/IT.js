4ba44108a2bbb246bdda3b31cfcd96d95b142315
sammy-j-hub <66547184+sammy-j-hub@users.noreply.github.com>
2020-07-08 10:36:09 +0530

    Delete IT.js

--- dialogs/IT.js
+++
@@ -1,7 +0,0 @@
-const ITAdaptiveCard = require('./navigationCards/itHelp.json');
-const {CardFactory } = require('botbuilder');
-
-function createITCard(){
-    return CardFactory.adaptiveCard(ITAdaptiveCard);
-}
-module.export = createITCard;
